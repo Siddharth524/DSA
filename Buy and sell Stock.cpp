b5d2f773744107bda50fae79b38150eefae4eca7
Siddharth524 <siddharthdaundkar2002@gmail.com>
2024-02-06 22:45:17 +0530

    Buy and sell Stock.cpp

---
+++ Buy and sell Stock.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int maxProfit=0;
+        int min_price=prices[0];
+        // for(int i =0 ;i<prices.size();i++){
+        //     for(int j = i;j<prices.size();j++){
+        //         int profit = prices[j]-prices[i];
+        //         maxProfit = max(profit,maxProfit);
+        //     }
+        // }
+        for(int i=1;i<prices.size();i++){
+            maxProfit = max(maxProfit,prices[i]-min_price);
+            min_price = min(prices[i],min_price);
+        }
+
+
+        
+        return maxProfit;
+    }
+};
